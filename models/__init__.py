dbd53306601955eb650806f420d5d924d4e445b4
cugitee <2289068115@qq.com>
2025-08-13 13:20:02 +0800

    11

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .dat_seg import DATSeg
 from .modules import DynamicBallQuery, AdaptiveAggregation, CSIT
-from .sgdat import SGDATSeg  # 新增 SGDATSeg 导入
+from .sgdat import SGDAT  # 新增 SGDATSeg 导入
 from .modules_sgdat import DynamicRadiusChannelFusion, ChannelCCC, LinearSpatialGVA  # 新增模块导入
 
 __all__ = [
@@ -8,7 +8,7 @@ __all__ = [
     'DynamicBallQuery',
     'AdaptiveAggregation',
     'CSIT',
-    'SGDATSeg',  # 新增 SGDATSeg
+    'SGDAT',  # 新增 SGDATSeg
     'DynamicRadiusChannelFusion',  # 新增模块
     'ChannelCCC',  # 新增模块
     'LinearSpatialGVA'  # 新增模块
